17b25875260064b1db2d1affd9bc4d246465d354
Aversiste <tristan.leguern@gmail.com>
2012-07-05 16:49:37 +0200

    replace ntohl with htonl

--- tuntap.c
+++ tuntap.c
@@ -242,7 +242,7 @@ tuntap_set_ip(struct device *dev, const char *saddr, int bits) {
 	/* Netmask */
 	mask = ~0;
 	mask = ~(mask >> bits);
-	mask = ntohl(mask);
+	mask = htonl(mask);
 
 	return tuntap_sys_set_ip(dev, addr, mask);
 }
