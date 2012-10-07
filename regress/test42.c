43387da6aab384bb93829cdcb00dfdbe7ff6f041
Aversiste <tristan.leguern@gmail.com>
2012-10-07 20:19:08 +0100

    Add three regression tests for tuntap_set_ifname()

---
+++ regress/test42.c
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2012 Tristan Le Guern <leguern AT medu DOT se>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <sys/types.h>
+
+#include <stdio.h>
+#if defined Windows
+# include <windows.h>
+#endif
+
+#include "tuntap.h"
+
+int
+main(void) {
+	int ret;
+	struct device *dev;
+
+	ret = 1;
+	dev = tuntap_init();
+	if (tuntap_start(dev, TUNTAP_MODE_ETHERNET, TUNTAP_ID_ANY) == -1)
+		ret = 1;
+
+	if (tuntap_set_ifname(dev, NULL) == -1) {
+		ret = 0;
+	}
+
+	tuntap_destroy(dev);
+	return ret;
+}
+
