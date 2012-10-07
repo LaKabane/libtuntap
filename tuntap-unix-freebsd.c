29a2225f9dad6df756e29948b9263d117856e52c
Aversiste <tristan.leguern@gmail.com>
2012-10-07 18:54:36 +0100

    Port to FreeBSD
    Regression tests seem to pass, but it's a lie.
    - tun devices might be not that usable
    - tun and tap devices are persistents by default
    - it's actualy not possible to destroy a device

--- tuntap-unix-freebsd.c
+++ tuntap-unix-freebsd.c
@@ -16,6 +16,7 @@
 
 #include <sys/types.h>
 #include <sys/ioctl.h>
+#include <sys/param.h> /* For MAXPATHLEN */
 #include <sys/socket.h>
 
 #include <arpa/inet.h>
@@ -26,6 +27,7 @@
 #include <netinet/in.h>
 
 #include <fcntl.h>
+#include <ifaddrs.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -34,19 +36,113 @@
 
 #include "tuntap.h"
 
-static int
-tuntap_sys_create_dev(struct device *dev, int tun) {
-	return -1;
-}
-
 int
 tuntap_sys_start(struct device *dev, int mode, int tun) {
-	return -1;
+	int fd;
+	int persist;
+	char *ifname;
+	char name[MAXPATHLEN];
+	struct ifaddrs *ifa;
+	struct ifreq ifr;
+
+	/* Get the persistence bit */
+	if (mode & TUNTAP_MODE_PERSIST) {
+		mode &= ~TUNTAP_MODE_PERSIST;
+		persist = 1;
+	} else {
+		persist = 0;
+	}
+
+        /* Set the mode: tun or tap */
+	if (mode == TUNTAP_MODE_ETHERNET) {
+		ifname = "tap";
+	} else if (mode == TUNTAP_MODE_TUNNEL) {
+		ifname = "tun";
+	} else {
+		tuntap_log(0, "Invalid mode for tuntap_start");
+		return -1;
+	}
+
+	/*
+	 * Try to use the given driver, or loop throught the avaible ones
+	 */
+	fd = -1;
+	if (tun < TUNTAP_ID_MAX) {
+		(void)snprintf(name, sizeof name, "/dev/%s%i", ifname, tun);
+		fd = open(name, O_RDWR);
+	} else if (tun == TUNTAP_ID_ANY) {
+		for (tun = 0; tun < TUNTAP_ID_MAX; ++tun) {
+			(void)memset(name, '\0', sizeof name);
+			(void)snprintf(name, sizeof name, "/dev/%s%i",
+			    ifname, tun);
+			if ((fd = open(name, O_RDWR)) > 0)
+				break;
+		}
+	} else {
+		return -1;
+	}
+
+	if (fd < 0 || fd == 256) {
+		(void)fprintf(stderr, "libtuntap (sys):"
+		    " Can't find a tun entry\n");
+		return -1;
+	}
+
+	/* Set the interface name */
+	(void)memset(&ifr, '\0', sizeof ifr);
+	(void)snprintf(ifr.ifr_name, sizeof ifr.ifr_name, "%s%i", ifname, tun);
+	/* And save it */
+	(void)strlcpy(dev->if_name, ifr.ifr_name, sizeof ifr.ifr_name);
+
+	/* Get the interface default values */
+	if (ioctl(dev->ctrl_sock, SIOCGIFFLAGS, &ifr) == -1) {
+		(void)fprintf(stderr, "libtuntap (sys): ioctl SIOCGIFFLAGS\n");
+		return -1;
+	}
+
+	/* Save flags for tuntap_{up, down} */
+	dev->flags = ifr.ifr_flags;
+
+	/* Save pre-existing MAC address */
+	if (getifaddrs(&ifa) == 0) {
+		struct ifaddrs *pifa;
+
+		for (pifa = ifa; pifa != NULL; pifa = pifa->ifa_next) {
+			if (strcmp(pifa->ifa_name, dev->if_name) == 0) {
+				struct ether_addr eth_addr;
+
+				/*
+				 * The MAC address is from 10 to 15.
+				 *
+				 * And yes, I know, the buffer is supposed
+				 * to have a size of 14 bytes.
+				 */
+				(void)memcpy(dev->hwaddr,
+				  pifa->ifa_addr->sa_data + 10,
+				  ETHER_ADDR_LEN);
+
+				(void)memset(&eth_addr.octet, 0,
+				  ETHER_ADDR_LEN);
+				(void)memcpy(&eth_addr.octet,
+				  pifa->ifa_addr->sa_data + 10,
+				  ETHER_ADDR_LEN);
+
+#if 0
+				fprintf(stderr,
+				  "MAC: %s\n", ether_ntoa(&eth_addr));
+#endif
+				break;
+			}
+		}
+		freeifaddrs(ifa);
+	}
+
+	return fd;
 }
 
 void
 tuntap_sys_destroy(struct device *dev) {
-	return -1;
+	return;
 }
 
 int
@@ -66,13 +162,10 @@ tuntap_sys_set_hwaddr(struct device *dev, struct ether_addr *eth_addr) {
 }
 
 int
-tuntap_sys_set_ipv4(struct device *dev, struct sockaddr_in *s4, uint32_t bits) {
-	struct ifaliasreq ifa;
+tuntap_sys_set_ipv4(struct device *dev, struct sockaddr_in *s, uint32_t bits) {
 	struct ifreq ifr;
 	struct sockaddr_in mask;
-
-	(void)memset(&ifa, '\0', sizeof ifa);
-	(void)strlcpy(ifa.ifra_name, dev->if_name, sizeof dev->if_name);
+	struct sockaddr_in addr;
 
 	(void)memset(&ifr, '\0', sizeof ifr);
 	(void)strlcpy(ifr.ifr_name, dev->if_name, sizeof dev->if_name);
@@ -80,30 +173,38 @@ tuntap_sys_set_ipv4(struct device *dev, struct sockaddr_in *s4, uint32_t bits) {
 	/* Delete previously assigned address */
 	(void)ioctl(dev->ctrl_sock, SIOCDIFADDR, &ifr);
 
-	/*
-	 * Fill-in the destination address and netmask,
-         * but don't care of the broadcast address
-	 */
-	(void)memcpy(&(ifa.ifra_addr), s4, sizeof ifa.ifra_addr);
+	/* Set the address */
+	(void)memset(&addr, '\0', sizeof addr);
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = s->sin_addr.s_addr;
+	addr.sin_len = sizeof addr;
+	(void)memcpy(&ifr.ifr_addr, &addr, sizeof addr);
 
+	if (ioctl(dev->ctrl_sock, SIOCSIFADDR, &ifr) == -1) {
+		tuntap_log(0, "libtuntap (sys): ioctl SIOCSIFADDR");
+		perror("ADDR");
+		return -1;
+	}
+
+	/* Then set the netmask */
 	(void)memset(&mask, '\0', sizeof mask);
 	mask.sin_family = AF_INET;
 	mask.sin_addr.s_addr = bits;
 	mask.sin_len = sizeof mask;
-	(void)memcpy(&ifa.ifra_mask, &mask, sizeof ifa.ifra_mask);
+	(void)memcpy(&ifr.ifr_addr, &mask, sizeof ifr.ifr_addr);
 
-	/* Simpler than calling SIOCSIFADDR and/or SIOCSIFBRDADDR */
-	if (ioctl(dev->ctrl_sock, SIOCAIFADDR, &ifa) == -1) {
-		tuntap_log(0, "libtuntap (sys): ioctl SIOCAIFADDR");
+	if (ioctl(dev->ctrl_sock, SIOCSIFNETMASK, &ifr) == -1) {
+		tuntap_log(0, "libtuntap (sys): ioctl SIOCSIFNETMASK");
+		perror("NETMASK");
 		return -1;
 	}
 	return 0;
 }
 
 int
-tuntap_sys_set_ipv6(struct device *dev, struct sockaddr_in6 *s6, uint32_t bits) {
+tuntap_sys_set_ipv6(struct device *dev, struct sockaddr_in6 *s, uint32_t bits) {
 	(void)dev;
-	(void)s6;
+	(void)s;
 	(void)bits;
 	tuntap_log(0, "libtuntap (sys): ipv6 not implemented");
 	return -1;
