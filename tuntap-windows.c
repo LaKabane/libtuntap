de5ade971f7993e73699e4c6b7fd98a394263485
cozis <heibyob@gmail.com>
2023-05-16 00:13:58 +0200

    bug fixes for windows

--- tuntap-windows.c
+++ tuntap-windows.c
@@ -122,7 +122,7 @@ reg_query(char *key_name) {
 			goto clean;
 		}
 		/* If its a tap adapter, its all good */
-		if (strncmp(data, "tap", 3) == 0) {
+		if (strncmp(data, "tap", 3) == 0 || strncmp(data, "root\\tap", 8) == 0) {
 			DWORD type;
 
 			len = sizeof data;
@@ -174,7 +174,7 @@ tuntap_start(struct device *dev, int mode, int tun) {
 
 	deviceid = reg_query(NETWORK_ADAPTERS);
 	snprintf(buf, sizeof buf, "\\\\.\\Global\\%s.tap", deviceid);
-	tun_fd = CreateFile(buf, GENERIC_WRITE | GENERIC_READ, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_SYSTEM|FILE_FLAG_OVERLAPPED, 0);
+	tun_fd = CreateFile(buf, GENERIC_WRITE | GENERIC_READ, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_SYSTEM | FILE_FLAG_OVERLAPPED, 0);
 	if (tun_fd == TUNFD_INVALID_VALUE) {
 		int errcode = GetLastError();
 
