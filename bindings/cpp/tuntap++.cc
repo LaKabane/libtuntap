e713e08a11cad9bb41ca51f4db2676df7313504e
Marek Küthe <m.k@mk16.de>
2023-12-21 14:44:06 +0000

    C++ bindings: Avoidance of code redundancy
    Signed-off-by: Marek Küthe <m.k@mk16.de>

--- bindings/cpp/tuntap++.cc
+++ bindings/cpp/tuntap++.cc
@@ -6,203 +6,102 @@
 
 namespace tuntap {
 
-tun::tun()
+tuntap::tuntap(int mode, int id)
     : _dev{tuntap_init()}, _started{true}
 {
-    if (tuntap_start(_dev, TUNTAP_MODE_TUNNEL, TUNTAP_ID_ANY) == -1) {
+    if (mode != TUNTAP_MODE_ETHERNET && mode != TUNTAP_MODE_TUNNEL) {
+        throw std::invalid_argument("Unknown tuntap mode");
+    }
+    if (id > TUNTAP_ID_MAX) {
+        throw std::invalid_argument("Tunnel ID too big");
+    }
+    if (tuntap_start(_dev, mode, id) == -1) {
         throw std::runtime_error("tuntap_start failed");
     }
 }
 
-tun::~tun()
+tuntap::~tuntap()
 {
     if (_started) {
         tuntap_destroy(_dev);
     }
 }
 
-tun::tun(tun &&t)
+tuntap::tuntap(tuntap &&t)
     : _dev(nullptr)
 {
     std::swap(t._dev, this->_dev);
 }
 
 void
-tun::release()
+tuntap::release()
 {
     tuntap_release(_dev);
     _started = false;
 }
 
 std::string
-tun::name() const
+tuntap::name() const
 {
     return tuntap_get_ifname(_dev);
 }
 
 void
-tun::name(std::string const &s)
+tuntap::name(std::string const &s)
 {
     tuntap_set_ifname(_dev, s.c_str());
 }
 
 t_tun
-tun::native_handle() const
+tuntap::native_handle() const
 {
     return tuntap_get_fd(this->_dev);
 }
 
 void
-tun::up()
+tuntap::up()
 {
     tuntap_up(_dev);
 }
 
 void
-tun::down()
+tuntap::down()
 {
     tuntap_down(_dev);
 }
 
 int
-tun::mtu() const
+tuntap::mtu() const
 {
     return tuntap_get_mtu(_dev);
 }
 
 void
-tun::mtu(int m)
+tuntap::mtu(int m)
 {
     tuntap_set_mtu(_dev, m);
 }
 
 void
-tun::ip(std::string const &s, int netmask)
+tuntap::ip(std::string const &s, int netmask)
 {
     tuntap_set_ip(_dev, s.c_str(), netmask);
 }
 
 int
-tun::read(void *buf, size_t len)
+tuntap::read(void *buf, size_t len)
 {
 	return tuntap_read(_dev, buf, len);
 }
 
 int
-tun::write(void *buf, size_t len)
+tuntap::write(void *buf, size_t len)
 {
 	return tuntap_write(_dev, buf, len);
 }
 
 void
-tun::nonblocking(bool b)
-{
-    tuntap_set_nonblocking(_dev, int(b));
-}
-
-tap::tap()
-    : _dev{tuntap_init()}, _started{true}
-{
-    if (tuntap_start(_dev, TUNTAP_MODE_ETHERNET, TUNTAP_ID_ANY) == -1) {
-        throw std::runtime_error("tuntap_start failed");
-    }
-}
-
-tap::~tap()
-{
-    if (_started) {
-        tuntap_destroy(_dev);
-    }
-}
-
-tap::tap(tap &&t)
-    : _dev(nullptr)
-{
-    std::swap(t._dev, this->_dev);
-}
-
-
-void
-tap::release()
-{
-    tuntap_release(_dev);
-    _started = false;
-}
-
-std::string
-tap::name() const
-{
-    return tuntap_get_ifname(_dev);
-}
-
-void
-tap::name(std::string const &s)
-{
-    tuntap_set_ifname(_dev, s.c_str());
-}
-
-std::string
-tap::hwaddr() const
-{
-    return tuntap_get_hwaddr(_dev);
-}
-
-void
-tap::hwaddr(std::string const &s)
-{
-    tuntap_set_hwaddr(_dev, s.c_str());
-}
-
-t_tun
-tap::native_handle() const
-{
-    return tuntap_get_fd(this->_dev);
-}
-
-void
-tap::up()
-{
-    tuntap_up(_dev);
-}
-
-void
-tap::down()
-{
-    tuntap_down(_dev);
-}
-
-int
-tap::mtu() const
-{
-    return tuntap_get_mtu(_dev);
-}
-
-void
-tap::mtu(int m)
-{
-    tuntap_set_mtu(_dev, m);
-}
-
-void
-tap::ip(std::string const &s, int netmask)
-{
-    tuntap_set_ip(_dev, s.c_str(), netmask);
-}
-
-int
-tap::read(void *buf, size_t len)
-{
-	return tuntap_read(_dev, buf, len);
-}
-
-int
-tap::write(void *buf, size_t len)
-{
-	return tuntap_write(_dev, buf, len);
-}
-
-void
-tap::nonblocking(bool b)
+tuntap::nonblocking(bool b)
 {
     tuntap_set_nonblocking(_dev, int(b));
 }
--- bindings/cpp/tuntap++.hh
+++ bindings/cpp/tuntap++.hh
@@ -8,54 +8,19 @@
 
 namespace tuntap {
 
-class tun
+class tuntap
 {
  public:
-  tun();
-  ~tun();
-  tun(tun const &) = delete;
-  tun & operator = (tun const &) = delete;
-  tun(tun &&);
+  tuntap(int, int = TUNTAP_ID_ANY);
+  ~tuntap();
+  tuntap(tuntap const &) = delete;
+  tuntap & operator = (tuntap const &) = delete;
+  tuntap(tuntap &&);
 
   // Properties
   std::string name() const;
   void name(std::string const &);
   int mtu() const ;
-  void mtu(int);
-  t_tun native_handle() const;
-
-  // Network
-  void up();
-  void down();
-  void ip(std::string const &presentation, int netmask);
-
-  //IO
-  int read(void *buf, size_t len);
-  int write(void *buf, size_t len);
-
-  // System
-  void release();
-  void nonblocking(bool);
- private:
-  struct device* _dev;
-  bool _started;
-};
-
-class tap
-{
- public:
-  tap();
-  ~tap();
-  tap(tap const &) = delete;
-  tap & operator = (tap const &) = delete;
-  tap(tap &&);
-
-  // Properties
-  std::string name() const;
-  void name(std::string const &);
-  std::string hwaddr() const;
-  void hwaddr(std::string const &);
-  int mtu() const;
   void mtu(int);
   t_tun native_handle() const;
 
