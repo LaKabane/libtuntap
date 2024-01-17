249109d4f78c8a5b8118684840f9a7040cf51033
Marek Küthe <m.k@mk16.de>
2024-01-17 00:41:49 +0000

    Improve error handling

--- deb/build.sh
+++ deb/build.sh
@@ -14,24 +14,23 @@ die () {
 (
     cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -B build -S ".." || die "Failed to configure project"
     cmake --build build || die "Failed to build project"
-    DESTDIR="../deb/package/" cmake --install build
+    DESTDIR="../deb/package/" cmake --install build || die "Failed to install project to package directory"
 )
 
-mkdir -p package/DEBIAN/
+mkdir -p package/DEBIAN/ || die "Failed to make create data directory"
 VERSION="$(git describe --long | sed 's/^libtuntap-//')"
 [ "$VERSION" ] || die "Failed to get version string"
 ARCH="$(dpkg --print-architecture)"
 [ "$ARCH" ] || die "Failed to get architecture string"
 
-cp control.template package/DEBIAN/control
-sed "s/%VERSION%/$VERSION/" -i package/DEBIAN/control
-sed "s/%ARCHITECTURE%/$ARCH/" -i package/DEBIAN/control
+cp control.template package/DEBIAN/control || die "Failed to copy control template"
+sed "s/%VERSION%/$VERSION/" -i package/DEBIAN/control || die "Failed to replace version in control template"
+sed "s/%ARCHITECTURE%/$ARCH/" -i package/DEBIAN/control || die "Failed to replace architecture in control template"
 
-mkdir -p package/usr/share/doc/libtuntap-dev/
-cp copyright package/usr/share/doc/libtuntap-dev/copyright
+mkdir -p package/usr/share/doc/libtuntap-dev/ || die "Failed to create package meta data directory"
+cp copyright package/usr/share/doc/libtuntap-dev/copyright || die "Failed to copy copyright file"
 
 echo "Building package..."
 dpkg-deb --root-owner-group --build package libtuntap-dev.deb || die "Failed to build package"
 
 echo "Package $(dpkg-deb --show libtuntap-dev.deb | sed "s/\t/ /") successful builded!"
-
