38566b70511a6b4073cbec435d0c003466bdbc13
Roland Schwarz OE1RSA <roland.schwarz@blackspace.at>
2026-04-20 14:54:23 +0200

    Add timeout option to cpp bindings

--- bindings/cpp/tuntap++.cc
+++ bindings/cpp/tuntap++.cc
@@ -94,15 +94,23 @@ tuntap::ip(std::string const &s, int netmask)
 }
 
 int
-tuntap::read(void *buf, std::size_t len) noexcept
+tuntap::read(void *buf, std::size_t len, int timeout_ms) noexcept
 {
-	return ::tuntap_read(_dev.get(), buf, len);
+	if (0 == timeout_ms) {
+		return ::tuntap_read(_dev.get(), buf, len);
+	} else {
+		return ::tuntap_read_tm(_dev.get(), buf, len, timeout_ms);
+	}
 }
 
 int
-tuntap::write(void *buf, std::size_t len) noexcept
+tuntap::write(void *buf, std::size_t len, int timeout_ms) noexcept
 {
-	return ::tuntap_write(_dev.get(), buf, len);
+	if (0 == timeout_ms) {
+		return ::tuntap_write(_dev.get(), buf, len);
+	} else {
+		return ::tuntap_write_tm(_dev.get(), buf, len, timeout_ms);
+	}
 }
 
 void
